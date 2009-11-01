0ba9d4e2bb200fce23c112fe56b40faebec7e6e8
pepe <pepe@dsic.upv.es>
2009-11-01 21:57:34 +0100

    Include function names in the trace

--- MonadLoc.hs
+++ MonadLoc.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE PatternGuards #-}
+
 import Control.Applicative
 import Data.Generics
 import Language.Haskell.Exts.Annotated
@@ -13,7 +15,7 @@ main = do
       mname  = case mhead of
                  Nothing -> ""
                  Just (ModuleHead _ mn _ _) -> prettyPrint mn
-      decls' = everywhere (mkT (annotateStatements mname)) decls
+      decls' = map (annotateDecl mname) decls
   writeFile outp $ prettyPrintStyleMode style{lineLength=100000} ourPrintMode mod'
 
 ourParseMode :: ParseMode
@@ -43,23 +45,33 @@ ourParseMode = defaultParseMode { extensions =
 ourPrintMode :: PPHsMode
 ourPrintMode = defaultMode { linePragmas = True }
 
-annotateStatements :: String -> Exp SrcSpanInfo -> Exp SrcSpanInfo
+annotateDecl :: String -> Decl SrcSpanInfo -> Decl SrcSpanInfo
+annotateDecl mname e@(FunBind loc (m:_)) = everywhere (mkT (annotateStatements (Just funName) mname)) e
+  where
+    funName | Match _ name _ _ _ <- m = prettyPrint name
+            | InfixMatch _ _ name _ _ _ <- m = prettyPrint name
 
-annotateStatements m (Do loc stmts)  = App loc (withLocCall m loc) (Paren loc $ Do  loc $ map (annotateStmt m) stmts)
-annotateStatements m (MDo loc stmts) = App loc (withLocCall m loc) (Paren loc $ MDo loc $ map (annotateStmt m) stmts)
-annotateStatements _ e = e
+annotateDecl mname e@(PatBind _ (PVar _ fn) _ _ _) = everywhere (mkT (annotateStatements (Just $ prettyPrint fn) mname)) e
+annotateDecl mname e = everywhere (mkT (annotateStatements Nothing mname)) e
 
-annotateStmt :: String -> Stmt SrcSpanInfo -> Stmt SrcSpanInfo
-annotateStmt m (Generator loc pat exp) = Generator loc pat $ App loc (withLocCall m loc) (Paren loc exp)
-annotateStmt m (Qualifier loc exp)     = Qualifier loc $ App loc (withLocCall m loc) (Paren loc exp)
-annotateStmt m (RecStmt loc stmts)     = RecStmt loc $ map (annotateStmt m) stmts
-annotateStmt _ stmt                    = stmt
+annotateStatements :: Maybe String -> String -> Exp SrcSpanInfo -> Exp SrcSpanInfo
+annotateStatements fun m (Do loc stmts)  = App loc (withLocCall fun m loc) (Paren loc $ Do  loc $ map (annotateStmt fun m) stmts)
+annotateStatements fun m (MDo loc stmts) = App loc (withLocCall fun m loc) (Paren loc $ MDo loc $ map (annotateStmt fun m) stmts)
+annotateStatements _   _ e = e
 
-withLocCall :: String -> SrcSpanInfo -> Exp SrcSpanInfo
-withLocCall m loc = App loc (Var loc withLoc) (Lit loc srclocLit)
+annotateStmt :: Maybe String ->  String -> Stmt SrcSpanInfo -> Stmt SrcSpanInfo
+annotateStmt fun m (Generator loc pat exp) = Generator loc pat $ App loc (withLocCall fun m loc) (Paren loc exp)
+annotateStmt fun m (Qualifier loc exp)     = Qualifier loc $ App loc (withLocCall fun m loc) (Paren loc exp)
+annotateStmt fun m (RecStmt loc stmts)     = RecStmt loc $ map (annotateStmt fun m) stmts
+annotateStmt _   _ stmt                    = stmt
+
+withLocCall :: Maybe String -> String -> SrcSpanInfo -> Exp SrcSpanInfo
+withLocCall fun m loc = App loc (Var loc withLoc) (Lit loc srclocLit)
   where
    withLoc   = Qual loc (ModuleName loc "Control.Monad.Loc") (Ident loc "withLoc")
    srclocLit = String loc (render locString) ""
-   locString = (text m <> parens(text (fileName loc)) <> colon <+> parens (int (startLine loc) <> comma <+> int(startColumn loc)))
-
+   locStringTail = text m <> parens(text (fileName loc)) <> colon <+> parens (int (startLine loc) <> comma <+> int(startColumn loc))
+   locString
+     | Nothing  <- fun = locStringTail
+     | Just fun <- fun = text fun <> comma <+> locStringTail
 
